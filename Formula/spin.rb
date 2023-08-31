f158c66a465d26ea1b0997a2d01e24d54afec1ba
Thorsten Hans <thorsten.hans@gmail.com>
2023-08-31 11:53:38 +0200

    chore: update spin formula to 1.4.2
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -1,26 +1,26 @@
 class Spin < Formula
   desc "Open-source tool for building and running serverless WebAssembly applications"
   homepage "https://developer.fermyon.com/spin"
-  version "1.4.1"
+  version "1.4.3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/fermyon/spin/releases/download/v1.4.1/spin-v1.4.1-macos-amd64.tar.gz"
-    sha256 "d42abb4dc79affb5cbd6a0f4e2bc2837675dca23aed9587a85f4d86672ec7f6f"
+    url "https://github.com/fermyon/spin/releases/download/v1.4.2/spin-v1.4.2-macos-amd64.tar.gz"
+    sha256 "4d1c6eb6ef7add631ca80206cc5b6aa9d9ad2bc9e4d79d111440db10e82f09f6"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fermyon/spin/releases/download/v1.4.1/spin-v1.4.1-macos-aarch64.tar.gz"
-    sha256 "98c3130468bb3e40b0cf6c94d3a746562fe2b6323d04e57cbfe3f753a8c5ccfe"
+    url "https://github.com/fermyon/spin/releases/download/v1.4.2/spin-v1.4.2-macos-aarch64.tar.gz"
+    sha256 "e519fac62ce3c830a83cac04ef0a409d155837c4334702a49951ceda258ae4ed"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fermyon/spin/releases/download/v1.4.1/spin-v1.4.1-linux-amd64.tar.gz"
-    sha256 "199881bf6c676d698e8fc910f7dc37aaa557d5db5ad2e080f65c48bac5fa786a"
+    url "https://github.com/fermyon/spin/releases/download/v1.4.2/spin-v1.4.2-linux-amd64.tar.gz"
+    sha256 "3298cb418aed5fd88b646b0bc6d1b2ceba171775dc323d76722c7010a11e0699"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/fermyon/spin/releases/download/v1.4.1/spin-v1.4.1-linux-aarch64.tar.gz"
-    sha256 "2ecff49db739ec8d3c1ee6409bedfcfe8a9b67aec3867bc4349a2c2d764291ce"
+    url "https://github.com/fermyon/spin/releases/download/v1.4.2/spin-v1.4.2-linux-aarch64.tar.gz"
+    sha256 "32cb6adac9a50162f5400741d5a3624c26f2ce7bc71fe63caad207a633a1a6af"
   end
 
   def install

b94f9d3e4cf83d5056f924920459074e65e539de
Vaughn Dice <vaughn.dice@fermyon.com>
2023-09-13 13:21:34 -0600

    chore(spin.rb): update to 1.5.0
    Signed-off-by: Vaughn Dice <vaughn.dice@fermyon.com>

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -1,26 +1,26 @@
 class Spin < Formula
   desc "Open-source tool for building and running serverless WebAssembly applications"
   homepage "https://developer.fermyon.com/spin"
-  version "1.4.2"
+  version "1.5.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/fermyon/spin/releases/download/v1.4.2/spin-v1.4.2-macos-amd64.tar.gz"
-    sha256 "4d1c6eb6ef7add631ca80206cc5b6aa9d9ad2bc9e4d79d111440db10e82f09f6"
+    url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-macos-amd64.tar.gz"
+    sha256 "989bd9e6def00b332038681697b388ea460a68b0ed5bf04bdebfce32e6bd0357"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fermyon/spin/releases/download/v1.4.2/spin-v1.4.2-macos-aarch64.tar.gz"
-    sha256 "e519fac62ce3c830a83cac04ef0a409d155837c4334702a49951ceda258ae4ed"
+    url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-macos-aarch64.tar.gz"
+    sha256 "48437366bc10c29e6fa0d917646c983d81cc0b1562cc5f37cda66f116a1247a0"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fermyon/spin/releases/download/v1.4.2/spin-v1.4.2-linux-amd64.tar.gz"
-    sha256 "3298cb418aed5fd88b646b0bc6d1b2ceba171775dc323d76722c7010a11e0699"
+    url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-linux-amd64.tar.gz"
+    sha256 "6c339d17e15eb2131db46c150a186bba3f8348d572fa9db8c9d83666e6bce559"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/fermyon/spin/releases/download/v1.4.2/spin-v1.4.2-linux-aarch64.tar.gz"
-    sha256 "32cb6adac9a50162f5400741d5a3624c26f2ce7bc71fe63caad207a633a1a6af"
+    url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-linux-aarch64.tar.gz"
+    sha256 "1d433c3cbdce020674d90eb5f7f43725e63595ad7cf3a95d4f13bc55b3f40b5a"
   end
 
   def install

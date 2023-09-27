091cf2bcafad058af7b5dbd8f0fdc4d0969eb4df
Michelle Dhanani <michelle@fermyon.com>
2023-09-27 11:13:55 -0400

    Update spin.rb with correct urls v1.5.1
    Signed-off-by: Michelle Dhanani <michelle@fermyon.com>

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -4,22 +4,22 @@ class Spin < Formula
   version "1.5.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-macos-amd64.tar.gz"
+    url "https://github.com/fermyon/spin/releases/download/v1.5.1/spin-v1.5.1-macos-amd64.tar.gz"
     sha256 "f19668ba1327edf0b2adac84796efc3ab0f86bda8fc072721ee1e2f741672174"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-macos-aarch64.tar.gz"
+    url "https://github.com/fermyon/spin/releases/download/v1.5.1/spin-v1.5.1-macos-aarch64.tar.gz"
     sha256 "1e397885a5a3219c67d46909ead26a42fe21bcac4abf58e3845e749071e3489a"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-linux-amd64.tar.gz"
+    url "https://github.com/fermyon/spin/releases/download/v1.5.1/spin-v1.5.1-linux-amd64.tar.gz"
     sha256 "31941a09c870f9bb3e74a068e1573bf7efb60bffdd3c555a1a0fd75d9fa1b217"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-linux-aarch64.tar.gz"
+    url "https://github.com/fermyon/spin/releases/download/v1.5.1/spin-v1.5.1-linux-aarch64.tar.gz"
     sha256 "d0c9a9903279f60fedb6425463c872066507923a1845cdead256d65705515b11"
   end
 

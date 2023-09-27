66ce8bb1de02c7213e66f1e30d3890f4e38545c0
Michelle Dhanani <michelle@fermyon.com>
2023-09-27 11:12:29 -0400

    Update spin.rb for v1.5.1
    Signed-off-by: Michelle Dhanani <michelle@fermyon.com>

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -1,26 +1,26 @@
 class Spin < Formula
   desc "Open-source tool for building and running serverless WebAssembly applications"
   homepage "https://developer.fermyon.com/spin"
-  version "1.5.0"
+  version "1.5.1"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-macos-amd64.tar.gz"
-    sha256 "989bd9e6def00b332038681697b388ea460a68b0ed5bf04bdebfce32e6bd0357"
+    sha256 "f19668ba1327edf0b2adac84796efc3ab0f86bda8fc072721ee1e2f741672174"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-macos-aarch64.tar.gz"
-    sha256 "48437366bc10c29e6fa0d917646c983d81cc0b1562cc5f37cda66f116a1247a0"
+    sha256 "1e397885a5a3219c67d46909ead26a42fe21bcac4abf58e3845e749071e3489a"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-linux-amd64.tar.gz"
-    sha256 "6c339d17e15eb2131db46c150a186bba3f8348d572fa9db8c9d83666e6bce559"
+    sha256 "31941a09c870f9bb3e74a068e1573bf7efb60bffdd3c555a1a0fd75d9fa1b217"
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/fermyon/spin/releases/download/v1.5.0/spin-v1.5.0-linux-aarch64.tar.gz"
-    sha256 "1d433c3cbdce020674d90eb5f7f43725e63595ad7cf3a95d4f13bc55b3f40b5a"
+    sha256 "d0c9a9903279f60fedb6425463c872066507923a1845cdead256d65705515b11"
   end
 
   def install

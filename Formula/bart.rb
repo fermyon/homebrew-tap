804eb20c9d6412d0032b1c4788ebd82f3e450d3e
Dimitri <5898931+dimitrilw@users.noreply.github.com>
2023-10-19 21:21:34 -0600

    add URLs & SHAs for Fermyon official 0.9.0 release binaries
    Signed-off-by: Dimitri <5898931+dimitrilw@users.noreply.github.com>

--- Formula/bart.rb
+++ Formula/bart.rb
@@ -1,37 +1,38 @@
 class Bart < Formula
   desc "The Micro-CMS for WebAssembly and Spin"
   homepage "https://developer.fermyon.com/bartholomew"
-  version "0.8.0"
+  version "0.9.0"
 
-  # TODO: replace the 'url' values with the actual release binaries; update 'sha256' values
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/fermyon/bartholomew/releases/download/v0.9.0/bart-v0.9.0-macos-amd64.tar.gz"
+    sha256 "0f4e5a3844b6d5ef4b7d33a50c69ddba002874da15d2821eea5736216c6ec0d9"
+  end
 
-  # if OS.mac? && Hardware::CPU.intel?
-  #   url "https://github.com/fermyon/bartholomew/releases/download/v0.8.0/bart-v0.8.0-macos-amd64.tar.gz"
-  #   sha256 "989bd9e6def00b332038681697b388ea460a68b0ed5bf04bdebfce32e6bd0357"
-  # end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/fermyon/bartholomew/releases/download/v0.9.0/bart-v0.9.0-macos-aarch64.tar.gz"
+    sha256 "85f30737b2eabd5426398b4edaf72d3454fed992d64c7dbeb2c630040588918b"
+  end
 
-  # if OS.mac? && Hardware::CPU.arm?
-  #   url "https://github.com/fermyon/bartholomew/releases/download/v0.8.0/bart-v0.8.0-macos-aarch64.tar.gz"
-  #   sha256 "48437366bc10c29e6fa0d917646c983d81cc0b1562cc5f37cda66f116a1247a0"
-  # end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/fermyon/bartholomew/releases/download/v0.9.0/bart-v0.9.0-linux-amd64.tar.gz"
+    sha256 "6a95803c3c62f67efccbaed1ebf506fdbe458f22e31fad66622b434265539d64"
+  end
 
-  # if OS.linux? && Hardware::CPU.intel?
-  #   url "https://github.com/fermyon/bartholomew/releases/download/v0.8.0/bart-v0.8.0-linux-amd64.tar.gz"
-  #   sha256 "6c339d17e15eb2131db46c150a186bba3f8348d572fa9db8c9d83666e6bce559"
-  # end
+  if OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/fermyon/bartholomew/releases/download/v0.9.0/bart-v0.9.0-linux-aarch64.tar.gz"
+    sha256 "5b031b864e03883ddb740aab8fd4ffaf306d043ba86f90bc5c123cef1ea7f006"  bart-v0.9.0-linux-aarch64.tar.gz
+  end
 
-  # if OS.linux? && Hardware::CPU.arm?
-  #   url "https://github.com/fermyon/bartholomew/releases/download/v0.8.0/bart-v0.8.0-linux-aarch64.tar.gz"
-  #   sha256 "1d433c3cbdce020674d90eb5f7f43725e63595ad7cf3a95d4f13bc55b3f40b5a"
-  # end
+  if OS.windows? # && Hardware::CPU.intel?
+    url "https://github.com/fermyon/bartholomew/releases/download/v0.9.0/bart-v0.9.0-windows-amd64.zip"
+    sha256 "9f0b067bbc17e0f85c4b9acd2bbc8f63c54b1dd2fa18153419a87ab95a676066"
+  end
 
   def install
     bin.install "bart"
   end
 
   test do
-    # system "#{bin}/bart", "--version"
-    # TODO: test & confirm this is proper syntax, then adjust spin.rb too
-    assert_match "bart 0.8.0", shell_output("#{bin}/bart --version")
+    assert_match "bart 0.9.0", shell_output("#{bin}/bart --version")
   end
 end

5868374ea1fe75b95a08f6b16a3e520d796b367e
Brian H <brian.hardock@fermyon.com>
2024-04-02 17:04:50 -0600

    Bump Spin Formula for Spin v2.4.1
    Signed-off-by: Brian H <brian.hardock@fermyon.com>

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -1,26 +1,26 @@
 class Spin < Formula
   desc "Open-source tool for building and running serverless WebAssembly applications"
   homepage "https://developer.fermyon.com/spin"
-  version "2.4.0"
+  version "2.4.1"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/fermyon/spin/releases/download/v#{version}/spin-v#{version}-macos-amd64.tar.gz"
-    sha256 "a9bcdc9be36fbd1185edd80572834cd13bdfe2dfc342259d138139d8a87297e5"
+    sha256 "08185095d77a5c1ec6028a47ea3db1851c2532fb5fd3603994490b14d2fc15a7"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/fermyon/spin/releases/download/v#{version}/spin-v#{version}-macos-aarch64.tar.gz"
-    sha256 "f9227e940fb5a75f7b77e132c6368f84155cecaae33f25db2f35810e0743d32c"
+    sha256 "5d832a96540132b89a178106ee0e864ccb0d6cc3587e6b9e06487a4042c72cca"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/fermyon/spin/releases/download/v#{version}/spin-v#{version}-linux-amd64.tar.gz"
-    sha256 "29df6109ae4a633bdaf3ce715dc38df96584635b55849ce1b68608b5f6a1dcc3"
+    sha256 "5da6484f3a437bd010f6b14d4fd8105952fd4afff347ead54704229cc3165dda"
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/fermyon/spin/releases/download/v#{version}/spin-v#{version}-linux-aarch64.tar.gz"
-    sha256 "d4cfa4fcc5c5a8f203674978948cded1ad5b8f6fbef798de0c02ebb55f3d0595"
+    sha256 "66bae58d986245905da21f3de758ae6c2e73d0af26bc26cb6cbd2a7d2fad5b4a"
   end
 
   def install
